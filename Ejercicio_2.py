c7356c7bb6d898bdbd1adbb54c0fa8dd6483bc29
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-12-14 11:26:55 +0100

    cambios

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -3,6 +3,11 @@ import sys
 import math
 from random import randint
 
+tablero = [
+['','',''],
+['','',''],       
+['','','']
+]
 
 def bloqueada (fila,columna):
 
@@ -13,19 +18,46 @@ def bloqueada (fila,columna):
          if tablero [fila + 1][columna ]!='' and tablero [fila - 1][columna ]!='':
              fail= True
 
-    else:
-            fail=False
 
     elif fila == 2 and tablero [fila - 1][columna] != ' ':
         fail= True
-
     else:
         fail = False
-
     return fail
 
 def printertbalero (tablero):
     contador_indice = 0
+    for tablero[contador_indice] in tablero:
+        print(tablero[contador_indice])
+        contador_indice += 1
+    print("\n")
+
+def movimiento(fila, columna):
+    if fila == 0:
+            tablero[fila+1][columna] = tablero[fila][columna]
+            tablero[fila][columna] = ' '
+    elif fila == 1:
+        if tablero[fila+1][columna] != ' ':
+            tablero[fila-1][columna] = tablero[fila][columna]
+            tablero[fila][columna] = ' '
+        else:
+            tablero[fila + 1][columna]= tablero [fila][columna]
+            tablero [fila][columna] = ' '
+    elif fila == 2:
+        tablero[fila - 1][columna]= tablero[fila][columna]
+        tablero [fila][columna] = ' '
+
+def cambio(fila, columna):
+    if fila == 0:
+        fila = fila + 1
+    elif fila == 1:
+        if tablero[fila+1][columna] != ' ':
+            fila = fila - 1
+        else:
+            fila = fila + 1
+    elif fila == 2:
+        fila = fila - 1
+    return fila
 
 while True:
 
